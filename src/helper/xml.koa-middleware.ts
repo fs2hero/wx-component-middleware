8bc0d6d1af0041a0bc9ddcb7ecbbf45584bcf5d3
beikebase <524696571@qq.com>
2021-09-26 17:15:27 +0800

    1. strapi 中间件

--- src/helper/xml.koa-middleware.ts
+++ src/helper/xml.koa-middleware.ts
@@ -1,6 +1,6 @@
 
 export default async function xmLMiddleWare(ctx, next) {
-  if (ctx.request.header['content-type']?.includes('xml')) {
+  if (ctx.request.header['content-type'] === 'application/xml') {
     // avoid conflict between xml and text
     ctx.request.header['content-type'] = 'application/xml'
     const xml: string = await new Promise((resolve) => {
@@ -9,6 +9,9 @@ export default async function xmLMiddleWare(ctx, next) {
       ctx.req.on('end', () => resolve(data));
     });
     ctx.request.xml = xml;
+  }
+  else if (ctx.request.header['content-type'] === 'text/xml') {
+    ctx.request.xml = ctx.request.body;
   }
   await next();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,76 @@
 import ComponentService from './helper/component-service';
 import tokenStore from './tokenStore/index';
-import {ClientOpts} from 'redis';
+// import { ClientOpts } from 'redis';
 import xmlMiddleWare from './helper/xml.koa-middleware';
-interface WX_C_Config {
-  componentAppId: string;
-  componentAppSecret: string;
-  token: string;
-  encodingAESKey: string;
-}
+// interface WX_C_Config {
+//   componentAppId: string;
+//   componentAppSecret: string;
+//   token: string;
+//   encodingAESKey: string;
+// }
 
-export interface wxc extends ComponentService {}
+export interface wxc extends ComponentService { }
 
-export default function wxComponentService(app: any,
-    wxConfig: WX_C_Config,
-    storeConfig?: ClientOpts,
-) {
-  let store: any = new tokenStore.MemoryStore();
+// export default function wxComponentService(app: any,
+//     wxConfig: WX_C_Config,
+//     storeConfig?: ClientOpts,
+// ) {
+//   let store: any = new tokenStore.MemoryStore();
 
-  if (storeConfig) {
-    store = new tokenStore.RedisStore(storeConfig);
+//   if (storeConfig) {
+//     store = new tokenStore.RedisStore(storeConfig);
+//   }
+
+//   const wxcCtx = new ComponentService(wxConfig, store);
+//   const wxcMiddleware = async (ctx, next) => {
+//     ctx.wxc = wxcCtx;
+//     await next();
+//   };
+//   app.use(xmlMiddleWare);
+//   app.use(wxcMiddleware);
+// }
+
+const wxComponentService =(strapi) => {
+  const middlewareName = 'wxopen'
+  const middlewareInitialisedLog = `${middlewareName} middleware has been initialised.`
+  const settingErrorLog = `${middlewareName} middleware configuration must contain a "settings" object. This object must contain at least the wxConfig property to initialise wxOpen SDK. Check the config/environments/**/middleware.json file.`
+
+  return {
+    initialize() {
+      // Get Strapi level middleware settings
+      const strapiMiddlewareSettings = strapi.config.middleware.settings
+      const { debug, settings } = strapiMiddlewareSettings[middlewareName]
+      // const environment = strapi.config.environment
+
+      if (
+        typeof settings === 'undefined' ||
+        typeof settings.wxConfig === 'undefined'
+      ) {
+        strapi.log.error(settingErrorLog)
+      } else {
+        // Initialise WxOpen SDK
+        // init({ ...settings, environment })
+        let store: any = new tokenStore.MemoryStore();
+
+        if (settings.storeConfig) {
+          store = new tokenStore.RedisStore(settings.storeConfig);
+        }
+
+        const wxcCtx = new ComponentService(settings.wxConfig, store);
+        const wxcMiddleware = async (ctx, next) => {
+          ctx.wxc = wxcCtx;
+          await next();
+        };
+        strapi.app.use(xmlMiddleWare);
+        strapi.app.use(wxcMiddleware);
+
+        // IF debug mode is on, let the user know if middleware was initialised
+        if (debug) {
+          strapi.log.info(middlewareInitialisedLog)
+        }
+      }
+    },
   }
-
-  const wxcCtx = new ComponentService(wxConfig, store);
-  const wxcMiddleware = async (ctx, next) => {
-    ctx.wxc = wxcCtx;
-    await next();
-  };
-  app.use(xmlMiddleWare);
-  app.use(wxcMiddleware);
 }
+
+module.exports = wxComponentService
